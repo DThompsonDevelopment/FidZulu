0a697cce05d1c42280a25ca31e0b5385dabeac42
Jon Riley <jon.riley019@gmail.com>
2022-04-18 15:14:36 -0400

    resolved index.js conflicts-- fixed post endpoint

--- backend/microservices/Food/Food/routes/index.js
+++ backend/microservices/Food/Food/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 const foodDataPath = "../../../../Foodjson.json";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -19,15 +19,15 @@ function readFoodDataSync() {
 }
 
 router.get('/food/team', (req, res, next) => {
-  res.json({team: "Food", membersNames: ["Jon Riley, Dillon Gorlesky, Dakota Thompson"]});
+  res.json({ team: "Food", membersNames: ["Jon Riley, Dillon Gorlesky, Dakota Thompson"] });
 })
 
 router.get('/food/all/:location', (req, res) => {
   let loc = req.params;
   let tax = 0;
-  if(/\b[Dd][Uu][Rr][Hh][Aa][Mm]\b/g.test(loc.location)){//Durham location
+  if (/\b[Dd][Uu][Rr][Hh][Aa][Mm]\b/g.test(loc.location)) {//Durham location
     tax = 0.075; //7.5% Tax
-  } else if(/\b[Rr][Aa][Ll][Ee][Ii][Gg][Hh]\b/g.test(loc.location)){//Raleigh location
+  } else if (/\b[Rr][Aa][Ll][Ee][Ii][Gg][Hh]\b/g.test(loc.location)) {//Raleigh location
     tax = 0.08; //8% Tax
   } else {
     //Throw error
@@ -35,39 +35,41 @@ router.get('/food/all/:location', (req, res) => {
   }
 
   let data = readFoodDataSync();
-  for(let jsonObj of data){
+  for (let jsonObj of data) {
     let price = jsonObj.price + (jsonObj.price * tax);
     jsonObj.price = price.toFixed(2);
   }
-  res.json(data);  
+  res.json(data);
 })
 
-//router.post();
 router.post('/food/add', (req, res, next) => {
 
   let foodStruct = {
-    name: req.query.name,
-    brand: req.query.brand,
-    weight: req.query.weight,
-    calories: req.query.calories,
-    price: req.query.price
+    name: req.body.name,
+    brand: req.body.brand,
+    weight: req.body.weight,
+    calories: Number(req.body.calories),
+    price: Number(req.body.price)
   }
 
-  // make sure struct is populated
-  for (let query in foodStruct) {
-    if (query) {
-      res.statusCode = 400;
-      res.write(`Error ${res.statusCode}: Bad query ${query}\n`);
-    }
-  }
+  // verify proper data type
+  if (!foodStruct.calories || !foodStruct.price) {
+    res.statusCode = 400;
+    res.json(
+      {
+        "error": "data type error",
+        "message": "Incorrect data type passed from body",
+      }
+    );
+  } else {
+    // modify foodjson to include new food item
+    let data = readFoodDataSync();
+    data.push(foodStruct)
+    fs.writeFileSync(foodDataPath, JSON.stringify(data));
 
-  // modify foodjson to include new food item
-  let data = readFoodDataSync();
-  data.push(newFoodItem)
-  fs.writeFileSync(foodDataPath, JSON.stringify(data));
-  
-  // respond with verified json 
-  res.json(newFoodItem);
+    // respond with verified json 
+    res.json(foodStruct);
+  }
 
 });
 
