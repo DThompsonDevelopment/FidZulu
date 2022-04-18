8511a904e4a0c5ee4563939c4fe6fc9637a5b53f
Hansen, Chris <chris.hansen@fmr.com>
2022-04-18 15:00:15 -0400

    Team Component in Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
 import { ReplaceUnderscoresPipe } from './pipes/replace-underscores.pipe';
 import { FormsModule } from '@angular/forms';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
-  declarations: [AppComponent, TableComponent, ReplaceUnderscoresPipe],
+  declarations: [AppComponent, TableComponent, ReplaceUnderscoresPipe, TeamComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
