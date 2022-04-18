018762e5b19866973174bc4d2a07220254f580d6
Dillon Gorlesky <dillongorlesky@gmail.com>
2022-04-18 14:28:42 -0400

    added get locations

--- backend/microservices/Food/Food/routes/index.js
+++ backend/microservices/Food/Food/routes/index.js
@@ -10,6 +10,22 @@ router.get('/food/team', (req, res, next) => {
   res.json({team: "Food", membersNames: ["Jon, Dillon, Dakota"]});
 })
 
-router.post();
+router.get('/food/all/:location', (req, res) => {
+  let loc = req.params;
+  let tax = 0;
+  if(/\b[Dd][Uu][Rr][Hh][Aa][Mm]\b/g.test(loc.location)){//Durham location
+    tax = 0.075; //7.5% Tax
+    console.log("durham inside")
+  } else if(/\b[Rr][Aa][Ll][Ee][Ii][Gg][Hh]\b/g.test(loc.location)){//Raleigh location
+    tax = 0.08; //8% Tax
+    console.log("raleigh inside")
+  } else {
+    //Throw error
+    console.log("else")
+  }
+  
+})
+
+//router.post();
 
 module.exports = router;
