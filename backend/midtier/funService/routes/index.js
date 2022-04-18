02751f9777169c0fa6857b8176963d7ca8ebe2ff
peteerian <erianpeto@gmail.com>
2022-04-18 16:20:47 -0400

    team endpoint test and impl

--- backend/midtier/funService/routes/index.js
+++ backend/midtier/funService/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/fun/team', function(req, res, next) {
+  const team = {
+    'team': 'Fun Team',
+    'membersnames': ['Vahe', 'Peter', 'Mike']
+};
+res.end(JSON.stringify(team));
 });
 
 module.exports = router;
--- backend/midtier/funService/spec/funServiceTeamMember_spec.js
+++ backend/midtier/funService/spec/funServiceTeamMember_spec.js
@@ -1,18 +1,28 @@
 let request = require("request");
-const base_url = "http://localhost:3021/fun";
-let team = require("../modules/team_names");
+const base_url = "http://localhost:3021/fun/";
+const team = {
+  'team': 'Fun Team',
+  'membersnames': ['Vahe', 'Peter', 'Mike']
+};
 console.log("Starting test");
 describe("First Node Test Server", () => {
   describe("GET /", () => {
     it("returns status code 200", (done) => {
-      request.get(base_url, (error, response, body) => {
+      request.get(base_url + "team", (error, response, body) => {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
     it("returns JSON of team names", (done) => {
       request.get(base_url + "team", (error, response, body) => {
-        expect(body).toBe(team);
+        expect(body).toBe(JSON.stringify(team));
+        done();
+      });
+
+    });
+    it("returns 404 error with wrong uri", (done) =>{
+      request.get(base_url + "tieams", (error,response,body) =>{
+        expect(response.statusCode).toBe(404);
         done();
       });
     });
