6111adb62a43bdb970b2a5cc68cbeabe65e0962f
Swapnil Kha <swapnilkha@gmail.com>
2022-04-18 13:29:19 -0400

    Update books.js

--- backend/microservices/Books/Books/modules/books.js
+++ backend/microservices/Books/Books/modules/books.js
@@ -10,24 +10,21 @@ exports.list = function() {
 
 exports.get_raleigh = function() {
     let json_result = JSON.parse(read_json_file());
-    let result = json_result.result;
-    console.log(result);
-    for (let i = 0; i < result.length; i ++){
-        let currBook = result[i] 
-        currBook["price"] = currBook["price"] * .925; //this doesn't work
-        return currBook;
+    for (let i = 0; i < json_result.length; i ++){
+        json_result[i].price = json_result[i].price * .925; //this doesn't work
+        return json_result[i];
     }
     return null;
 }
 
-exports.get_durham = function() {
-    let json_result = JSON.parse(read_json_file());
-    let result = json_result.result;
-    for (let i = 0; i < result.length; i ++){
-        let currBook = result[i]
-        currBook["price"] = currBook["price"] * .92; //this doesn't work
-        return currBook;
-    }
-    return null;
-}
+// exports.get_durham = function() {
+//     let json_result = JSON.parse(read_json_file());
+//     let result = json_result.result;
+//     for (let i = 0; i < result.length; i ++){
+//         let currBook = result[i]
+//         currBook.price = currBook.price * .92; //this doesn't work
+//         return currBook;
+//     }
+//     return null;
+// }
 
