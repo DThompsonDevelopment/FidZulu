8c39e053064a9217b08ba416335fb06512bc3454
Swapnil Kha <swapnilkha@gmail.com>
2022-04-18 15:05:15 -0400

    updates tests

--- backend/microservices/Books/Books/spec/booktest.spec.js
+++ backend/microservices/Books/Books/spec/booktest.spec.js
@@ -1,5 +1,8 @@
 let request = require("request");
 const base_url = "http://localhost:3034/books/all";
+const r_url = "http://localhost:3034/books/all/Raleigh";
+const d_url =  "http://localhost:3034/books/all/Durham";
+const bookteam_url = "http://localhost:3034/books/book-team";
 console.log("Starting test");
 
 describe("Testing Get /", () => {
@@ -60,14 +63,14 @@ describe("Testing Get /", () => {
 
     describe("GET /raleigh", () => {
         it("returns status code 200", (done) => {
-            request.get(base_url, (error, response, body) => {
+            request.get(r_url, (error, response, body) => {
                 expect(response.statusCode).toBe(200);
                 done();
             });
         }); 
 
         it("Testing Length", (done) => {
-            request.get(base_url, (error, response, body) => {
+            request.get(r_url, (error, response, body) => {
                 parsed_body = JSON.parse(body);
                 expect(parsed_body.length).toBe(4);
                 done();
@@ -75,18 +78,40 @@ describe("Testing Get /", () => {
         });
 
         it("Testing First Book Exists", (done) => {
-            request.get(base_url, (error, response, body) => {
+            request.get(r_url, (error, response, body) => {
                 parsed_body = JSON.parse(body)[0];
                 expect(parsed_body["Title"]).toBe('Lord of the Rings');
                 expect(parsed_body["Author"]).toBe('J.R.R Tolkien');
-                expect(parsed_body["price"]).toBe(25.99);
+                expect(parsed_body["price"]).toBe(27.94);
                 expect(parsed_body["ISBN"]).toBe('9780261102385');
                 expect(parsed_body["publisher"]).toBe('HarperCollins');
                 done();
             });
         });
 
-        
+        it("Testing Second Book Exists", (done) => {
+            request.get(r_url, (error, response, body) => {
+                parsed_body = JSON.parse(body)[1];
+                expect(parsed_body["Title"]).toBe('The Hobbit');
+                expect(parsed_body["Author"]).toBe('J.R.R Tolkien');
+                expect(parsed_body["price"]).toBe(10.62);
+                expect(parsed_body["ISBN"]).toBe('0261102214');
+                expect(parsed_body["publisher"]).toBe('HarperCollins');
+                done();
+            });
+        });
+
+        it("Testing Third Book Exists", (done) => {
+            request.get(r_url, (error, response, body) => {
+                parsed_body = JSON.parse(body)[2];
+                expect(parsed_body["Title"]).toBe("Lord of Souls");
+                expect(parsed_body["Author"]).toBe("Greg Keyes");
+                expect(parsed_body["price"]).toBe(13.95);
+                expect(parsed_body["ISBN"]).toBe("0345508025");
+                expect(parsed_body["publisher"]).toBe("Del Rey");
+                done();
+            });
+        });
         
 
     });
@@ -96,18 +121,18 @@ describe("Testing Get /", () => {
     describe("GET /Durham", () => {
 
         it("returns status code 200", (done) => {
-            request.get(base_url, (error, response, body) => {
+            request.get(d_url, (error, response, body) => {
                 expect(response.statusCode).toBe(200);
                 done();
             });
         }); 
 
         it("Testing First Book", (done) => {
-            request.get(base_url, (error, response, body) => {
+            request.get(d_url, (error, response, body) => {
                 parsed_body = JSON.parse(body)[0];
                 expect(parsed_body["Title"]).toBe('Lord of the Rings');
                 expect(parsed_body["Author"]).toBe('J.R.R Tolkien');
-                expect(parsed_body["price"]).toBe(23.91);
+                expect(parsed_body["price"]).toBe(28.07);
                 expect(parsed_body["ISBN"]).toBe('9780261102385');
                 expect(parsed_body["publisher"]).toBe('HarperCollins');
                 done();
@@ -115,17 +140,44 @@ describe("Testing Get /", () => {
         });
 
         it("Testing Second Book", (done) => {
-            request.get(base_url, (error, response, body) => {
+            request.get(d_url, (error, response, body) => {
                 parsed_body = JSON.parse(body)[1];
                 expect(parsed_body["Title"]).toBe('The Hobbit');
                 expect(parsed_body["Author"]).toBe('J.R.R Tolkien');
-                expect(parsed_body["price"]).toBe(9.09);
+                expect(parsed_body["price"]).toBe(10.67);
                 expect(parsed_body["ISBN"]).toBe('0261102214');
                 expect(parsed_body["publisher"]).toBe('HarperCollins');
                 done();
             });
         });
 
+
+    });
+
+    describe("GET /book-team", () => {
+
+        it("returns team info", (done) => {
+            request.get(bookteam_url, (error, response, body) => {
+                expect(body).toContain("team");
+                expect(body).toContain("membersNames");
+                done();
+            });
+        });
+
+        it("returns correct team name", (done) => {
+            request.get(bookteam_url, (error, response, body) => {
+                expect(body).toContain("Book Team");
+                done();
+            });
+        });
+
+        it("returns correct team members", (done) => {
+            request.get(bookteam_url, (error, response, body) => {
+                expect(body).toContain("Swapnil Kha");
+                expect(body).toContain("Eric Vo");               
+                done();
+            });
+        });
 
     });
 
