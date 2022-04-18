9333ccca34013800e0a59d276166f336cc7b6ae6
bigyan313 <bigyan.adhikari313@gmail.com>
2022-04-18 14:09:53 -0400

    /dvd/team json done

--- backend/microservices/DVDs/DvdService/routes/index.js
+++ backend/microservices/DVDs/DvdService/routes/index.js
@@ -2,12 +2,32 @@
 const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
-const contacts = require('../modules/dvd');
+const dvds = require('../modules/dvd');
+const dvdteam = require('../modules/team');
+
 const url = require('url');
 
 router.get('/ping', (request, response, next) => {
  response.send('Hello From Service');
 });
 
+
+// router.get('/dvdtest/team', (request, response, next) => {
+//     response.send('Returns all json object with team name and all team members names');
+//    });
+   
+   router.get('/dvd/team', (request, response, next) => {
+
+   // let get_params = url.parse(request.url, true).query;
+
+    if (Object.keys(dvdteam).length ==0 ){
+
+        next(createError(204));
+    
+    }else{
+        console.log('got into dvd');
+        response.end(JSON.stringify(dvdteam.list()));
+    }
+  });
 
 module.exports = router;
