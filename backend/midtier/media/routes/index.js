1cb7c9dbab8070588da79f2b85ec5a149f3b80af
dev4848 <devrc13@gmail.com>
2022-04-18 16:23:01 -0400

    added axios.get function

--- backend/midtier/media/routes/index.js
+++ backend/midtier/media/routes/index.js
@@ -20,24 +20,34 @@ router.get('/media/team', (req, res, next) => {
 // books endpoint
 router.get('/media/books/all/:location', async function(req, res, next) {
   const location = req.params.location
-  let endpoint_res = await axios.get('', {
-    'params': {
-      'location': location
-    }
-  });
+  if(Object.keys(req.params).length === 0) {
+    console.log("No params");
+  }
+  else if(Object.keys(req.params).length > 1) {
+    console.log("Too many params");
+  }
+  else if(location === undefined) {
+    console.log("Undefined location");
+  }
+
+  let endpoint_res = axios.get('https://localhost:3034/books/all/' + location).then(r => r.data);
   res.end(endpoint_res);
 });
 
 //dvds endpoint
-router.get('/media/dvds/all/:location', function(req, res, next) {
+router.get('/media/dvds/all/:location', async function(req, res, next) {
   const location = req.params.location
-  let params = url.parse(request.url, true).query;
+  let endpoint_res = await axios.get('https://localhost:3034/dvds/all/' + location);
+  res.end(endpoint_res.data);
+  // let params = url.parse(request.url, true).query;
 });
 
 //laptops endpoint
-router.get('/media/laptops/all/:location', function(req, res, next) {
+router.get('/media/laptops/all/:location', async function(req, res, next) {
   const location = req.params.location
-  let params = url.parse(request.url, true).query;
+  let endpoint_res = await axios.get('https://localhost:3034/laptops/all/' + location);
+  res.end(endpoint_res.data);
+  // let params = url.parse(request.url, true).query;
 });
 
 console.log('Listening on port 3022');
