06886e1e85b8230607c486b747dbc872bbbcad70
mrbellyrubs <jlin07744@gmail.com>
2022-04-18 12:08:21 -0400

    initial all bikes endpoint.

--- backend/microservices/Bikes/routes/index.js
+++ backend/microservices/Bikes/routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+const createError = require("http-errors");
+var express = require("express");
 var router = express.Router();
 
+const fs = require("fs");
+
+const salesTax = {
+  raleigh: 0.075,
+  durham: 0.08,
+};
+
+let read_json_file = () => {
+  let file = "./data/Bikejson.json";
+  return fs.readFileSync(file);
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/bikes/all/:location", function (req, res, next) {
+  const param = req.params.location;
+
+  if (!Object.keys(salesTax).includes(param)) {
+    console.log(`${param} not in`);
+    next(createError(204));
+  } else {
+    console.log("got into /bikes/all/:location " + param);
+
+    const bikes = JSON.parse(read_json_file());
+    res.send(bikes);
+  }
 });
 
 module.exports = router;
