96fcbab215863fd50f63eeef2665bcda40659034
Yahia <yahia.berrada@fmr.com>
2022-04-18 12:11:28 -0400

    service updated + some tests

--- src/app/service/fidzula.service.ts
+++ src/app/service/fidzula.service.ts
@@ -7,6 +7,7 @@ import { Toy } from '../models/toy';
 import { Book } from '../models/books';
 import { Dvd } from '../models/dvd';
 import { Laptop } from '../models/laptop';
+import { Team } from '../models/teams';
 
 @Injectable({
   providedIn: 'root'
@@ -34,8 +35,8 @@ export class FidzulaService {
   public getBikesWithLocation(location:string) : Observable<Bike[]> {
     return this.http.get<Bike[]>(this.bikes + location).pipe(catchError(this.handleError));
   }
-  public getBikesWithTeam() : Observable<Bike[]> {
-    return this.http.get<Bike[]>(this.bikes + "team").pipe(catchError(this.handleError));
+  public getBikesWithTeam() : Observable<Team> {
+    return this.http.get<Team>(this.bikes + "team").pipe(catchError(this.handleError));
   }
   public addBike(bike:Bike) : Observable<Bike> {
     return this.http.post<Bike>(this.bikes + "add", bike).pipe(catchError(this.handleError));
@@ -45,8 +46,8 @@ export class FidzulaService {
   public getFoodsWithLocation(location:string) : Observable<Food[]> {
     return this.http.get<Food[]>(this.food + location).pipe(catchError(this.handleError));
   }
-  public getFoodWithTeam() : Observable<Food[]> {
-    return this.http.get<Food[]>(this.food + "team").pipe(catchError(this.handleError));
+  public getFoodWithTeam() : Observable<Team> {
+    return this.http.get<Team>(this.food + "team").pipe(catchError(this.handleError));
   }
   public addFood(food:Food) : Observable<Food> {
     return this.http.post<Food>(this.food + "add", food).pipe(catchError(this.handleError));
@@ -56,8 +57,8 @@ export class FidzulaService {
   public getToysWithLocation(location:string) : Observable<Toy[]> {
     return this.http.get<Toy[]>(this.toys + location).pipe(catchError(this.handleError));
   }
-  public getToyWithTeam() : Observable<Toy[]> {
-    return this.http.get<Toy[]>(this.toys + "team").pipe(catchError(this.handleError));
+  public getToyWithTeam() : Observable<Team> {
+    return this.http.get<Team>(this.toys + "team").pipe(catchError(this.handleError));
   }
   public addToy(toy:Toy) : Observable<Toy> {
     return this.http.post<Toy>(this.toys + "add", toy).pipe(catchError(this.handleError));
@@ -69,8 +70,8 @@ export class FidzulaService {
   public getBooksWithLocation(location:string) : Observable<Book[]> {
     return this.http.get<Book[]>(this.books + location).pipe(catchError(this.handleError));
   }
-  public getBookWithTeam() : Observable<Book[]> {
-    return this.http.get<Book[]>(this.books + "team").pipe(catchError(this.handleError));
+  public getBookWithTeam() : Observable<Team> {
+    return this.http.get<Team>(this.books + "team").pipe(catchError(this.handleError));
   }
   public addBook(book:Book) : Observable<Book> {
     return this.http.post<Book>(this.books + "add", book).pipe(catchError(this.handleError));
@@ -80,8 +81,8 @@ export class FidzulaService {
   public getDvdsWithLocation(location:string) : Observable<Dvd[]> {
     return this.http.get<Dvd[]>(this.dvds + location).pipe(catchError(this.handleError));
   }
-  public getDvdWithTeam() : Observable<Dvd[]> {
-    return this.http.get<Dvd[]>(this.dvds + "team").pipe(catchError(this.handleError));
+  public getDvdWithTeam() : Observable<Team> {
+    return this.http.get<Team>(this.dvds + "team").pipe(catchError(this.handleError));
   }
   public addDvd(dvd:Dvd) : Observable<Dvd> {
     return this.http.post<Dvd>(this.dvds + "add", dvd).pipe(catchError(this.handleError));
@@ -91,8 +92,8 @@ export class FidzulaService {
   public getLaptopsWithLocation(location:string) : Observable<Laptop[]> {
     return this.http.get<Laptop[]>(this.laptops + location).pipe(catchError(this.handleError));
   }
-  public getLaptopWithTeam() : Observable<Laptop[]> {
-    return this.http.get<Laptop[]>(this.laptops + "team").pipe(catchError(this.handleError));
+  public getLaptopWithTeam() : Observable<Team> {
+    return this.http.get<Team>(this.laptops + "team").pipe(catchError(this.handleError));
   }
   public addLaptop(laptop:Laptop) : Observable<Laptop> {
     return this.http.post<Laptop>(this.laptops + "add", laptop).pipe(catchError(this.handleError));
