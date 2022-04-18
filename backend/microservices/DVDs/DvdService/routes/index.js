c8d1425e2863196d41d76ab390cc36093450b0b0
Pawan <pawan.adhikari2336@gmail.com>
2022-04-18 14:27:43 -0400

    added get alldvds method

--- backend/microservices/DVDs/DvdService/routes/index.js
+++ backend/microservices/DVDs/DvdService/routes/index.js
@@ -7,9 +7,29 @@ const dvdteam = require('../modules/team');
 
 const url = require('url');
 
+const { response } = require('../app');
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'DvdService' });
+  });
 router.get('/ping', (request, response, next) => {
  response.send('Hello From Service');
 });
+
+router.get("/dvds/all/:location", (req,res,next) => {
+    const param = req.params.location;
+    console.log('got into dvd/all/:location ' + param);
+    if (Object.keys(param).length == 0) {
+        console.log('no params');
+       next(createError(204));
+      } 
+      else{
+        res.end(JSON.stringify(dvds.query_with_salesTax(param)));
+      }
+
+})
 
 
 // router.get('/dvdtest/team', (request, response, next) => {
