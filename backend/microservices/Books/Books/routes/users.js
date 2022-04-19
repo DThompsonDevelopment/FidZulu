db6de6737ad9640d8e52fff1d81ce7d741347ba0
Swapnil Kha <swapnilkha@gmail.com>
2022-04-19 09:41:43 -0400

    Update users.js

--- backend/microservices/Books/Books/routes/users.js
+++ backend/microservices/Books/Books/routes/users.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/books/add', function(req, res) {
+  try {
+    res.send({
+      'Title': req.body.Title,
+      'Author': req.body.Author,
+      'price': req.body.price,
+      "ISBN": req.body.ISBN
+    });
+  } catch (err) {
+    nextends(createError(500));
+  }
+    
+});
+
 module.exports = router;
