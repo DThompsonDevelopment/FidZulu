edadbc1950bb884289642584835ab6af7509fa17
zackschulman <schulman279@gmail.com>
2022-04-19 11:46:16 -0400

    added tests for post method

--- backend/microservices/Toys/Toys/spec/location_e2e_spec.js
+++ backend/microservices/Toys/Toys/spec/location_e2e_spec.js
@@ -1,11 +1,18 @@
 const request = require("request");
-
+let location = require("../modules/location");
 const base_url = 'http://localhost:3033/';
 const contacts_url = base_url + 'toys/all/raleigh';
-
+var fs = require('fs')
 
 
 describe("First Node Test Server", () => {
+
+    afterEach(() =>{
+        let read = fs.readFileSync('../../../../Toysjson.json');
+        let parsed = JSON.parse(read);
+        fs.writeFileSync('./data/toys.json', JSON.stringify(parsed));
+    });
+
     describe("GET /toys/all/:location", () => {
         it("returns 21.94 for Fisher-Price Medical Kit", function (done) {
             request.get("http://localhost:3033/toys/all/raleigh",
@@ -65,6 +72,33 @@ describe("First Node Test Server", () => {
             request.get("http://localhost:3033/toys/team",
                 (error, response, body) => {
                     expect(response.statusCode).toBe(200);
+                    done();
+                });
+        });
+    });
+    describe("POST /toys/add", () => {
+        it("adds the specified toy", function (done) {
+            var url = "http://localhost:3033/toys/add"
+            request({
+                url: url,
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: {
+                    name:"Test Toy",
+                    brand:"Fisher-Price",
+                    age_group:"3 to 9",
+                    price:20.41
+                },
+                json: true
+            },()=> {expect(location.list().length).toBe(5); done();} );
+   
+        });
+        it("returns 201 for valid post", (done) => {
+            request.post("http://localhost:3033/toys/add",
+                (error, response, body) => {
+                    expect(response.statusCode).toBe(201);
                     done();
                 });
         });
